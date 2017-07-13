08568a55a6a56feef7e4c5533c5d39ea7e7b4c3d
nl <646417754@qq.com>
2017-07-13 22:38:49 +0800

    mater delete

--- jeenl-parent/jeenl-web/src/test/java/com/jeenl/controller/UserServiceTest.java
+++ jeenl-parent/jeenl-web/src/test/java/com/jeenl/controller/UserServiceTest.java
@@ -46,23 +46,5 @@ QName opAddEntry = new QName("http://impl.service.jeenl.com","login");
         serviceClient.invokeRobust(opAddEntry, opAddEntryArgs);;
     }  
     public static void test3() throws Exception{  
-QName opAddEntry = new QName("http://impl.service.jeenl.com","login");  
-        // 指定sayHelloToPerson方法的参数值  
-        Object[] opAddEntryArgs = new Object[] { "username","password" };  
-	哈哈哈
-        // 指定sayHelloToPerson方法返回值的数据类型的Class对象  
-        Class[] classes = new Class[] { String.class };  
-        // 调用sayHelloToPerson方法并输出该方法的返回值  
-//        serviceClient.invokeBlocking(opAddEntry,opAddEntryArgs, classes);
-        // 使用RPC方式调用WebService  
-        RPCServiceClient serviceClient = new RPCServiceClient();  
-        Options options = serviceClient.getOptions();  
-        // 指定调用WebService的URL  
-        EndpointReference targetEPR = new EndpointReference("http://localhost:8080/jeenl-web/services/userService");  
-        options.setTo(targetEPR);  
-          
-        // 指定要调用的sayHelloToPerson方法及WSDL文件的命名空间  
-        
-        serviceClient.invokeRobust(opAddEntry, opAddEntryArgs);;
     }  
 }  
